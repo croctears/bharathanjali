73afb8b02837767e2f3c56588c8db862e8c1c2de
Zimam Sayeed <zimamsayeed@Zimams-MacBook-Pro.local>
2024-06-14 13:57:35 +0530

    Added upploadphotos lambda

---
+++ lambda/uploadphotos.js
@@ -0,0 +1,33 @@
+const AWS = require('aws-sdk');
+const s3 = new AWS.S3();
+
+exports.handler = async (event) => {
+    const bucketName = '';
+    const body = JSON.parse(event.body);
+    const base64String = body.file.split(';base64,').pop();
+    const buffer = Buffer.from(base64String, 'base64');
+    const fileName = body.fileName;
+    const filePath = `uploads/${fileName}`;
+    
+    const params = {
+        Bucket: bucketName,
+        Key: filePath,
+        Body: buffer,
+        ContentEncoding: 'base64',
+        ContentType: 'image/jpeg'
+    };
+    
+    try {
+        await s3.putObject(params).promise();
+        const url = `https://${bucketName}.s3.amazonaws.com/${filePath}`;
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ url })
+        };
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: error.message })
+        };
+    }
+};
