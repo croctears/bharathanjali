9cc2513e864e41dfb69b2b14dee80de42be4a262
Zimam Sayeed <zimamsayeed@Zimams-MacBook-Pro.local>
2024-06-14 14:23:25 +0530

    Updated lmabda

--- lambda/uploadphotos.js
+++ lambda/uploadphotos.js
@@ -1,8 +1,21 @@
-const AWS = require('aws-sdk');
-const s3 = new AWS.S3();
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 
-exports.handler = async (event) => {
-    const bucketName = '';
+const s3 = new S3Client({ region: "ap-south-1" });
+
+export const handler = async (event) => {
+    const bucketName = 'bharathanjali';
+    if (event.httpMethod === 'OPTIONS') {
+        return {
+            statusCode: 200,
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Headers': 'Content-Type',
+                'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+            },
+            body: ''
+        };
+    }
+
     const body = JSON.parse(event.body);
     const base64String = body.file.split(';base64,').pop();
     const buffer = Buffer.from(base64String, 'base64');
@@ -18,7 +31,8 @@ exports.handler = async (event) => {
     };
     
     try {
-        await s3.putObject(params).promise();
+        const command = new PutObjectCommand(params);
+        await s3.send(command);
         const url = `https://${bucketName}.s3.amazonaws.com/${filePath}`;
         return {
             statusCode: 200,
